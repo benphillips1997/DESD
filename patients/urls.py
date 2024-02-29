2c6d92f0c22d2d9d8c351e2a9662609b37bb0987
unknown <filip2.orczynski@live.uwe.ac.uk>
2024-02-29 00:27:24 +0000

    Home and Login Page Implemented

--- patients/urls.py
+++ patients/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from patients import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home/", views.home, name="home"),
     path("login/", views.login, name="login"),
     path("user_login/", views.user_login, name="user_login"),
     path("sign_up/", views.sign_up, name="sign_up"),
